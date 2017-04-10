ff2295ec3ef2224dbec5e61a7a735d66af64787e
Billgo <cocobill@vip.qq.com>
2017-04-10 15:00:11 +0800

    update store

--- client/app/stores/production.js
+++ client/app/stores/production.js
@@ -1,25 +1,25 @@
 import { createStore, applyMiddleware } from 'redux';
-import { routerMiddleware, syncHistoryWithStore } from 'react-router-redux';
+import { routerMiddleware } from 'react-router-redux';
 import { fetchMiddleware } from 'teasim-middlewares';
 import { rootReducer } from 'app/reducers/index';
 import thunkMiddleware from 'redux-thunk';
 
 export default function generateStore(baseHistory, initialState) {
   const routingMiddleware = routerMiddleware(baseHistory);
-
+  
   const middlewares = applyMiddleware(
     thunkMiddleware,
     fetchMiddleware,
-    routingMiddleware,
+    routingMiddleware
   );
 
   const store = createStore(
     rootReducer,
     initialState,
-    middlewares,
+    middlewares
   );
 
-  const history = syncHistoryWithStore(baseHistory, store);
+  const history = baseHistory;
 
   return { store, history };
-};
+}
